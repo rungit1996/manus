93d1315202d478e31e6cb1acf81f3bbc82372a51
iysz <iyszhu@163.com>
2026-05-02 15:53:20 +0800

    日志扩展集成

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,18 @@
+import logging
+
 from fastapi import FastAPI
 
+from app.infrastructure.logging import setup_logging
 from core.config import get_settings
 
-# 加载配置信息
+# 1. 加载配置信息
 settings = get_settings()
+
+# 2. 初始化日志系统
+setup_logging()
+logger = logging.getLogger()
+
+logger.info("测试日志系统")
 
 print(settings)
 
